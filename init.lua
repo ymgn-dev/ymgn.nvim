aa2102e685ae032d6bc09c04f6b8427ad6367485
ymgn-dev <ymgn.dev@gmail.com>
2023-08-28 14:55:14 +0900

    feat: telescope-file-browser.nvim

--- init.lua
+++ init.lua
@@ -136,6 +136,12 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'nvim-telescope/telescope-file-browser.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    event = 'VeryLazy',
+  },
+
   { import = 'core.plugins' },
 
   { import = 'custom.plugins' },
@@ -180,9 +186,25 @@ require('telescope').setup {
       },
     },
   },
+  extensions = {
+    file_browser = {
+      theme = 'ivy',
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        ['i'] = {
+          -- your custom insert mode mappings
+        },
+        ['n'] = {
+          -- your custom normal mode mappings
+        },
+      },
+    },
+  },
 }
 
 pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'file_browser')
 
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
@@ -199,6 +221,8 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+vim.api.nvim_set_keymap('n', '<space>fb', ':telescope file_browser<CR>', { noremap = true })
 
 require('nvim-treesitter.configs').setup {
   ensure_installed = {
