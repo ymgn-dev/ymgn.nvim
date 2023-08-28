b75d09138404b96d03b33d607b2c93334bdaa0f3
ymgn-dev <ymgn.dev@gmail.com>
2023-08-28 17:47:58 +0900

    feat: add Format keymap

--- init.lua
+++ init.lua
@@ -200,6 +200,11 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set('n', ';', ':')
 
+vim.keymap.set({ 'n', 'v' }, '<leader>fm', function()
+  require('format-on-save').format()
+  require('format-on-save').restore_cursors()
+end, { desc = '[F]r[M]at', silent = true })
+
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
