75220cec2989362f2887312c0eefbe7ebbbf8e8b
ymgn-dev <ymgn.dev@gmail.com>
2023-09-03 21:44:51 +0900

    refactor: options

--- init.lua
+++ init.lua
@@ -220,10 +220,6 @@ vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.wo.signcolumn = 'yes'
 
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
